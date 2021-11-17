2000703c9ee66b76a68ab00efaaeeb557a5dcd22
andre-cyb <andrea.vg.malfatti@gmail.com>
2021-11-17 19:38:15 +0100

    Milestone 2: completata

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <template>
   <div id="app">
     <input type="search" v-model="userInput" />
-    <button @click="movieCall">Search</button>
+    <button @click="movieCall(`movie`, `movies`), movieCall(`tv`, `series`)">
+      Search
+    </button>
     <ul>
       <li v-for="(movie, i) in movies" :key="i">
         <ul class="lista_film pb-5">
-          <li>{{ movie.title }}</li>
+          <li>
+            <strong>{{ movie.title }}</strong>
+          </li>
           <li>{{ movie.original_title }}</li>
-          <li>{{ movie.original_language }}</li>
+          <li>{{ lang[movie.original_language] }}</li>
           <li>{{ movie.vote_average }}</li>
+        </ul>
+      </li>
+    </ul>
+    <ul>
+      <li v-for="(serie, i) in series" :key="i">
+        <ul class="lista_film pb-5">
+          <li>
+            <strong>{{ serie.name }}</strong>
+          </li>
+          <li>{{ serie.original_name }}</li>
+          <li>{{ lang[serie.original_language] }}</li>
+          <li>{{ serie.vote_average }}</li>
         </ul>
       </li>
     </ul>
@@ -25,30 +41,40 @@ export default {
     return {
       apiKey: "1fef8132a8e5f3adf9eced61ab7c59c8",
       apiUrl: "https://api.themoviedb.org/3",
+      imgUrl: "https://image.tmdb.org/t/p/",
       movies: [],
+      series: [],
+
       userInput: "",
+      lang: {
+        it: "bandiera italiana",
+        en: "bandiera inglese",
+        fr: "bandiera francese",
+        de: "bandiera tedesca",
+      },
     };
   },
 
   methods: {
-    movieCall() {
+    movieCall(type, objData) {
       if (!this.userInput) {
         return;
       }
       axios
-        .get(`${this.apiUrl}/search/movie`, {
+        .get(this.apiUrl + `/search/` + type, {
           params: {
             api_key: this.apiKey,
-            query: this.userInput, //collega con v-model, è il titolo del fim
+            query: this.userInput,
             languages: "en",
           },
         })
         .then((resp) => {
-          this.movies = resp.data.results;
+          this[objData] = resp.data.results;
         });
     },
+    thumbnailCall() {},
   },
-  computed: {},
+  mounted() {},
 };
 </script>
 
@@ -62,8 +88,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-  /* .lista_film {
-    padding-bottom: 20px;
-  } */
 }
 </style>
